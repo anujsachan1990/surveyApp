accfe9217ca47bba1c1a840876c2c9b579b316bf
ANUJ SACHAN <anuj.sachan@akqa.com>
2019-11-22 11:25:14 +1100

    correct typo

--- src/app/ErrorBoundary.js
+++ src/app/ErrorBoundary.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 
-export class ErrorBoundary extends PureComponent {
+class ErrorBoundary extends PureComponent {
   static getDerivedStateFromError() {
     // Update state so the next render will show the fallback UI.
     return { hasError: true }
